b4eaeb6e0ab65e1768f1fecb9670dffe08a93df6
Divishkumar28 <divish.karthikeyan@gmail.com>
2024-01-11 10:17:56 +0530

    New_portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Education from './components/Education';
+import Experience from './components/Experience';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PORTFOLIO</h1>
+      <Experience/>
+      <Education/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Education.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div className='education'>
+        <div className='graduation'>
+            <h1>B.E,CSE</h1>
+            <h2>Adhiyamaan College of Engineering, Hosur</h2>
+            <h4>CGPA = 7.8 </h4>
+        </div>
+        <div className='hslc'>
+            <h1>HSLC</h1>
+            <h2>Sri Vijayvidyalaya Matric Hr Sec School, Hosur</h2>
+            <h4>CGPA = 7.1 </h4>
+        </div>
+        <div className='sslc'>
+            <h1>SSLC</h1>
+            <h2>Parimalam Matric Hr Sec School, Hosur</h2>
+            <h4>CGPA = 10 </h4>
+        </div>
+    </div>
+  )
+}
+
+export default Education
